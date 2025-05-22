27a0fea32844ba849c5776c89fb6e1bed894bd75
Vitaog <vitaoog@gmail.com>
2025-05-21 22:52:40 -0300

    feat: Adição de validação de keywords no mongodb

--- app.py
+++ app.py
@@ -11,10 +11,11 @@ app = Flask(__name__)
 
 NESTJS_BASE_URL = "http://localhost:3003"
 MONGO_URI = os.getenv("MONGO_URI")
-
 mongo_client = MongoClient(MONGO_URI)
 mongo_db = mongo_client.get_database("Cluster0")
 keywords_collection = mongo_db["keywords"]
+
+keywords_collection.create_index("keyword", unique=True)
 
 @app.route('/extrair-keywords', methods=['POST'])
 def extrair_keywords():
@@ -28,14 +29,27 @@ def extrair_keywords():
         return jsonify({"erro": "Campos obrigatórios: chamado_id, descricao, tipo_importacao"}), 400
 
     resultado_completo = extrair_keywords_com_embeddings(descricao, top_n=3)
+
     palavras_chave = [item["keyword"] for item in resultado_completo]
 
     try:
-        documentos = [{"keyword": item["keyword"], "embedding": item["embedding"]} for item in resultado_completo]
-        if documentos:
-            keywords_collection.insert_many(documentos)
+        for item in resultado_completo:
+            keyword = item["keyword"]
+
+            existe = keywords_collection.find_one({"keyword": keyword})
+
+            if not existe:
+                keywords_collection.insert_one({
+                    "keyword": keyword,
+                    "embedding": item["embedding"]
+                })
+            else:
+                print(f"Keyword '{keyword}' já existe no MongoDB. Ignorando inserção.")
     except Exception as e:
-        return jsonify({"erro": "Erro ao salvar no MongoDB", "detalhes": str(e)}), 500
+        return jsonify({
+            "erro": "Erro ao salvar no MongoDB",
+            "detalhes": str(e)
+        }), 500
 
     try:
         response = requests.patch(
@@ -45,7 +59,10 @@ def extrair_keywords():
         )
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
-        return jsonify({"erro": "Falha ao atualizar keywords no backend", "detalhes": str(e)}), 500
+        return jsonify({
+            "erro": "Falha ao atualizar keywords no backend",
+            "detalhes": str(e)
+        }), 500
 
     return jsonify({
         "chamado_id": chamado_id,
